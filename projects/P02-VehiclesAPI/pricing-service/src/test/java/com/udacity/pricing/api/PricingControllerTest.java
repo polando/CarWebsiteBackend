d78015cafca6d1244356e1e9f663f8b54ea698f0
polando <cpt.m.qasemi@gmail.com>
2021-03-08 19:03:15 +0100

    Major Fix

---
+++ projects/P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/api/PricingControllerTest.java
@@ -0,0 +1,46 @@
+package com.udacity.pricing.api;
+
+import com.udacity.pricing.domain.price.Price;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class PricingControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void testGetPriceById() {
+        ResponseEntity<Price> response =
+                restTemplate.getForEntity("http://localhost:" + port + "/services/price?vehicleId=1", Price.class);
+
+        Assert.assertEquals(response.getStatusCode(), HttpStatus.OK);
+    }
+
+}
